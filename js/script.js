a37668e94112140f33d9960ca25ff9b18e72bfc8
Willhax0813 <Wa8312407@gmail.com>
2023-04-16 16:31:16 +0700

    membuat portfolio pertama

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const navbarNav = document.querySelector(".navbar-nav");
-
+const menu = document.querySelector("#menu");
 document.querySelector("#menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
+
+menu = (icon) => icon.classList.toggle(".fi fi-br-cross");
